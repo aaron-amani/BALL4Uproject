616a50c609e386307c4d2d840b464230ca4e3ef1
Sollres <48085114+Sollres@users.noreply.github.com>
2021-04-10 17:52:21 +0200

    Update joueur.component.ts

--- ball4Ufront/src/app/components/joueur/joueur.component.ts
+++ ball4Ufront/src/app/components/joueur/joueur.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class JoueurComponent implements OnInit {
 
   constructor() { 
-      var reqURL = "https://kdbsports-nbaplayers.p.rapidapi.com/players/team?name=lakers";
+      var reqURL = "https://kdbsports-nbaplayers.p.rapidapi.com/players/team?name=lakers";//Lien de  l'API
       const request = new XMLHttpRequest();
       
       request.onreadystatechange = function() {
@@ -17,16 +17,16 @@ export class JoueurComponent implements OnInit {
             const image = document.querySelectorAll('img.joueurs');
             const h2 = document.querySelectorAll('h2.joueurs');
             var response = JSON.parse(this.responseText);
-            putSliderImage(response,image,h2,main);
+            putSliderImage(response,image,h2,main);//Créer le slide contenant les joueurs de l'equipe
             
         }
       };
     
   
-      request.open("GET", reqURL);
-      request.setRequestHeader("X-RapidAPI-Key", "a863ab8093mshc096f61f89dd46ap18df74jsne41a36463df8");
-      request.setRequestHeader("X-Rapidapi-Host", "kdbsports-nbaplayers.p.rapidapi.com");
-      request.send();
+      request.open("GET", reqURL);//Ouvre le lien de l'API
+      request.setRequestHeader("X-RapidAPI-Key", "a863ab8093mshc096f61f89dd46ap18df74jsne41a36463df8");//Utilise la clé de l'API
+      request.setRequestHeader("X-Rapidapi-Host", "kdbsports-nbaplayers.p.rapidapi.com");//Utilise l'host 
+      request.send();//Envoie une requete
   
     
   }
@@ -40,15 +40,15 @@ export class JoueurComponent implements OnInit {
 function putSliderImage(response: any,image:any,h2:any,main:any) {
 
 
-
+ //Affiche chaque joueurs de l'équipe (response.length)
  for (let i = 0; i <response.length ; i++) {
   
   var span = document.createElement("span");
   span.setAttribute("class","sportsmagazine-color");
-  span.setAttribute("style","text-color:white;");
-  image[i].setAttribute("src", response[i].headShotUrl);
-  h2[i].appendChild(document.createTextNode(response[i].jerseyNumber+" "+response[i].firstName+" "+response[i].lastName));
-
+  span.setAttribute("style","text-color:white;");//Choix de la couleur du texte
+  image[i].setAttribute("src", response[i].headShotUrl);//Attribut l'image correspondante
+  h2[i].appendChild(document.createTextNode(response[i].jerseyNumber+" "+response[i].firstName+" "+response[i].lastName));//Attache les infos relatives au joueurs
+  //Infos complementaire au joueurs sont ajoutées ici
   main[i].appendChild(span.appendChild(document.createTextNode("Position :"+response[i].position+" /  Taille (en ft) : "+response[i].height+"  /   Moyenne Points :"+response[i].careerPoints+"  / Moyenne contres: "+response[i].careerBlocks+"/  Moyenne PasseD: "+response[i].carrerAssists+" /  Moyenne Rebonds :"+response[i].careerRebounds)));
 
   }
