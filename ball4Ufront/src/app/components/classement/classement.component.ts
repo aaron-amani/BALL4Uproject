a156e475c5cde57e9c3e8c6f3d318eee3e439068
Sollres <48085114+Sollres@users.noreply.github.com>
2021-04-10 17:48:01 +0200

    Update classement.component.ts

--- ball4Ufront/src/app/components/classement/classement.component.ts
+++ ball4Ufront/src/app/components/classement/classement.component.ts
@@ -9,7 +9,7 @@ export class ClassementComponent implements OnInit {
 
   constructor() {
 
-
+    //lien de requete de l'API
     var reqURL = "https://api-basketball.p.rapidapi.com/standings?league=12&season=2020-2021&group=Western%20Conference";
     const request = new XMLHttpRequest();
     
@@ -17,15 +17,16 @@ export class ClassementComponent implements OnInit {
       if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
         var main = document.querySelectorAll(".ranking-content-row");
         var response = JSON.parse(this.responseText);
+        //Affiche le classement
         getClassement(response,main);
       }
     };
   
-
-    request.open("GET", reqURL);
-    request.setRequestHeader("X-RapidAPI-Key", "a863ab8093mshc096f61f89dd46ap18df74jsne41a36463df8");
-    request.setRequestHeader("X-Rapidapi-Host", "api-basketball.p.rapidapi.com");
-    request.send();
+    //Realise la requete
+    request.open("GET", reqURL); //Lien
+    request.setRequestHeader("X-RapidAPI-Key", "a863ab8093mshc096f61f89dd46ap18df74jsne41a36463df8");//Cle de la requete
+    request.setRequestHeader("X-Rapidapi-Host", "api-basketball.p.rapidapi.com");//Host de la requete
+    request.send();//Envoie de la requete
 
    }
 
@@ -35,9 +36,9 @@ export class ClassementComponent implements OnInit {
 }
 
 function getClassement(response: any,main:any) {
-
+  //affiche les 15 equipes du classement
   for (let i = 0; i < 15; i++) {
-
+    //Declaration des variables
     var positionLi = document.createElement('li');
     var imageLi = document.createElement('li');
     var totalMatchLi = document.createElement('li');
@@ -49,24 +50,24 @@ function getClassement(response: any,main:any) {
 
     var positionTxt=document.createTextNode(response.response[0][i].position);
     var logo = document.createElement('img');
-    logo.src=response.response[0][i].team.logo;
+    logo.src=response.response[0][i].team.logo;//Recuperation du logo de l'équipe
     
     var totalMatchTxt=document.createTextNode(response.response[0][i].games.played);
     var totalGagnerTxt=document.createTextNode(response.response[0][i].games.win.total);
     var TotalPointTxt=document.createTextNode(response.response[0][i].games.lose.total);
     span.appendChild(document.createTextNode(response.response[0][i].team.name));
 
-
+    //Attache les différents elements du classemtn
     div.setAttribute("class","ranking-logo");
     div.appendChild(span);
-    
+    //ci dessous attache le texte au componant
     positionLi.appendChild(positionTxt);
-    imageLi.appendChild(logo);
+    imageLi.appendChild(logo);//Logo
     imageLi.appendChild(div);
-    totalMatchLi.appendChild(totalMatchTxt);
-    totalGagnerLi.appendChild(totalGagnerTxt);
-    TotalPointLi.appendChild(TotalPointTxt);
-
+    totalMatchLi.appendChild(totalMatchTxt);//Total de matchs joués
+    totalGagnerLi.appendChild(totalGagnerTxt);//Total de matchs gagnés
+    TotalPointLi.appendChild(TotalPointTxt);//Total de points
+    
     main[i].appendChild(positionLi);
     main[i].appendChild(imageLi);
     main[i].appendChild(totalMatchLi);
