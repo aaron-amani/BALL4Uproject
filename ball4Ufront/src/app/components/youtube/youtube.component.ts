0daa57311110cba885184ffc60d04cdf01596200
Sollres <48085114+Sollres@users.noreply.github.com>
2021-04-10 18:06:51 +0200

    Update youtube.component.ts

--- ball4Ufront/src/app/components/youtube/youtube.component.ts
+++ ball4Ufront/src/app/components/youtube/youtube.component.ts
@@ -9,10 +9,10 @@ export class YoutubeComponent implements OnInit {
 
   constructor() { 
 
-    var channelID = "UC8CSt-oVqy8pUAoKSApTxQw";
-    var reqURL = "https://www.youtube.com/feeds/videos.xml?channel_id=";
+    var channelID = "UC8CSt-oVqy8pUAoKSApTxQw";//id de la chaine des lakers
+    var reqURL = "https://www.youtube.com/feeds/videos.xml?channel_id=";//url permettant de recuperer une chaine youtube
     var request = new XMLHttpRequest();
-    
+    //creation d'un iframe qui contiendra la video
     request.onreadystatechange = function() {
       if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
           var video = document.querySelectorAll("iframe#video");
@@ -20,6 +20,7 @@ export class YoutubeComponent implements OnInit {
           getVideo(response,video);
       }
   };
+    //ouverture de la requette et envoi
   request.open("GET","https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent(reqURL)+channelID);
   request.send();
 
@@ -29,9 +30,9 @@ export class YoutubeComponent implements OnInit {
   }
 
 }
-
+//Fonction pour recuperer les videos
 function getVideo(response: any,video: any) {
-
+  //recupere l'id des video a afficher et creer un lien
   for (let i = 0; i < video.length; i++) {
     var link = response.items[i].link;
     var id = link.substr(link.indexOf("=")+1);
