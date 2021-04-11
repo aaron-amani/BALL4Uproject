de5981d25b6ec3ad813627bc3ab2b0efd311bc92
Aaron Amani <45314036+kobra5639@users.noreply.github.com>
2021-04-11 15:53:41 +0200

    Update ville.component.ts

--- ball4Ufront/src/app/components/ville/ville.component.ts
+++ ball4Ufront/src/app/components/ville/ville.component.ts
@@ -13,9 +13,10 @@ export class VilleComponent implements OnInit {
     //Creation du slider contenant les images obtenues par la requete
     request.onreadystatechange = function() {
         if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-            const ville = document.querySelector('div.sportsmagazine-featured-slider');
+            const image = document.querySelectorAll('img.ville');
+            const h2 = document.querySelectorAll('h2.tags');
             var response = JSON.parse(this.responseText);
-            showImage(response,ville);
+            showImage(response,image,h2);
         }
     };
     request.open("GET","https://pixabay.com/api/?key=15492974-061bbda5374d4a3662d9709c3&q=Los+Angeles&image_type=photo&pretty=true");//Ouverture d'un requete
@@ -28,34 +29,18 @@ export class VilleComponent implements OnInit {
   
 }
 
-function showImage(response: any,ville:any) {
+function showImage(response: any,image:any,h2:any) {
   //On réalise l'opération un nombre de fois égal au nombre d'images voulues
-  for (let i = 0; i < 25; i++) {
-    //Creation des variables 
-    var div1 = document.createElement('div');
-    var span = document.createElement('span');
-    var image = document.createElement('img');
-    var div2 = document.createElement('div');
-    var h2 = document.createElement('h2');
+  for (let i = 0; i < 10; i++) {
 
-    
-
-    div1.className="sportsmagazine-featured-slider-layer";//Attribution du nom de la class utilisée dans le code html
-
-    div2.className="sportsmagazine-featured-caption";//Attribution du nom de la classe utilisée dans le code html
-    image.src=response.hits[i].largeImageURL;//Recuperation de l'image et attribution dans la variable 
-    span.className ="sportsmagazine-black-transparent";//Attribution du nom de la classe utilisée dans le code html
-    
-    //Attache les attributions réalisées plus haut dans les variables
-    div1.appendChild(image);
-    div1.appendChild(span);
-    h2.appendChild(document.createTextNode(response.hits[i].tags));
-    div2.appendChild(h2);
-    div1.appendChild(div2);
-    ville.appendChild(div1);
+    image[i].src=response.hits[i].largeImageURL;//Attribut l'image correspondante
+    h2[i].appendChild(document.createTextNode(response.hits[i].tags));//Attache les infos relatives au lieu de la ville
+  
   }
   
 
 }
+
+
 
 
