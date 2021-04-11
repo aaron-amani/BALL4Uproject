8399d9334b243131541ef974746963367cd500d2
Aaron Amani <45314036+kobra5639@users.noreply.github.com>
2021-04-11 15:04:26 +0200

    Update calendrier-match.component.ts

--- ball4Ufront/src/app/components/calendrier-match/calendrier-match.component.ts
+++ ball4Ufront/src/app/components/calendrier-match/calendrier-match.component.ts
@@ -5,11 +5,32 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './calendrier-match.component.html',
   styleUrls: ['./calendrier-match.component.css']
 })
-export class CalendrierMatchComponent implements OnInit {
 
-  constructor() { }
+export class CalendrierMatchComponent implements OnInit {
+  
+  loadAPI: Promise<any>;
+  
+  constructor() { 
+      const url = 'https://tv-sports.fr/widget.js'; 
+      this.loadAPI = new Promise((resolve) => {
+          console.log('resolving promise...');
+          loadScript(url);
+      });
+    
+  }
 
   ngOnInit(): void {
   }
-
+  
 }
+
+function loadScript(url:any) {
+  console.log('preparing to load...')
+  let node = document.createElement('script');
+  node.src = url;
+  node.type = 'text/javascript';
+  node.async = true;
+  node.charset = 'utf-8';
+  document.getElementsByTagName('head')[0].appendChild(node);
+}
+
